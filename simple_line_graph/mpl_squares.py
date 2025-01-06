585f2dceae259f8ccbbfc48a9949486681dea512
emiliano <echiper.dev@gmail.com>
2025-01-06 12:32:59 +0200

    Plot a series of points with scatter()

--- simple_line_graph/mpl_squares.py
+++ simple_line_graph/mpl_squares.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
-print(plt.style.available)
 plt.style.use('seaborn-v0_8-notebook')
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
