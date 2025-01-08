af3bbe690eb9614dc87f29fffcc6cef745f2a592
emiliano <echiper.dev@gmail.com>
2025-01-08 12:05:09 +0200

    Add automatic data calculation logic

--- simple_line_graph/scatter_squares.py
+++ simple_line_graph/scatter_squares.py
@@ -2,12 +2,12 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -16,5 +16,8 @@ ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(labelsize=14)
+
+# Set range for each axis.
+ax.axis([0, 1100, 0, 1_100_000])
 
 plt.show()
