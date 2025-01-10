7f6e52b8601296e589a79cc3a631ccbf4d00b3e8
emiliano <echiper.dev@gmail.com>
2025-01-10 11:13:31 +0200

    Add colorsmap to plot

--- cubes.py
+++ cubes.py
@@ -7,7 +7,7 @@ y_values = [x**3 for x in x_values]
 
 plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
 
 ax.set_title("Cubes", fontsize=32)
 ax.set_xlabel("Value", fontsize=16)
@@ -15,7 +15,8 @@ ax.set_ylabel("Cube", fontsize=16)
 
 ax.tick_params(labelsize=16)
 
-ax.axis([0, 5100, 0, 126_000_000_000])
+ax.axis([0, 5100, 0, 130_000_000_000])
 ax.ticklabel_format(style='plain')
 
-plt.show()
+plt.savefig('cubes_plot.png', bbox_inches='tight')
+
