9c9703f4ed224bb9d3ee687cf2e8cb859afd207e
emiliano <echiper.dev@gmail.com>
2025-01-10 11:08:43 +0200

    Plot the first 5000 cubes with the scatter() method

--- cubes.py
+++ cubes.py
@@ -2,9 +2,20 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-cubes = [1, 8, 27, 64, 125]
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
 
+plt.style.use('Solarize_Light2')
 fig, ax = plt.subplots()
-ax.plot(cubes)
+ax.scatter(x_values, y_values, s=10)
+
+ax.set_title("Cubes", fontsize=32)
+ax.set_xlabel("Value", fontsize=16)
+ax.set_ylabel("Cube", fontsize=16)
+
+ax.tick_params(labelsize=16)
+
+ax.axis([0, 5100, 0, 126_000_000_000])
+ax.ticklabel_format(style='plain')
 
 plt.show()
