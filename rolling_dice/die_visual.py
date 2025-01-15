dd5366d566f24de0b06b02631d239ba178f84de5
emiliano <echiper.dev@gmail.com>
2025-01-15 17:57:31 +0200

    Make a histogram using Plotly's express module

--- rolling_dice/die_visual.py
+++ rolling_dice/die_visual.py
@@ -1,3 +1,5 @@
+import plotly.express as px
+
 from die import Die
 
 # Create a D6.
@@ -15,6 +17,10 @@ poss_results = range(1, die.num_sides+1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
+
+# Visualize the results.
+fig = px.bar(x=poss_results, y=frequencies)
+fig.show()
 
 print(frequencies)
 
