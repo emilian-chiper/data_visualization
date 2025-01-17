faccf63084191db912d7129b028ac8545045d1f7
emiliano <echiper.dev@gmail.com>
2025-01-17 13:17:42 +0200

    Add hover text

--- eq_data/eq_world_map.py
+++ eq_data/eq_world_map.py
@@ -11,20 +11,24 @@ all_eq_data = json.loads(contents)
 # Examine all earthquakes in the dataset.
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    eq_titles.append(eq_title)
 
 title = 'Global Earthquakes'
 fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
                      color=mags,
                      color_continuous_scale='Magma',
                      labels={'color':'Magnitude'},
-                     projection='natural earth')
+                     projection='natural earth',
+                     hover_name=eq_titles,
+                     )
 fig.show()
 
