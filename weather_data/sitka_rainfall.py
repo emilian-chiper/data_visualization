66f155025e3a53924b4cb6cb70307de9b2b55721
emiliano <echiper.dev@gmail.com>
2025-01-17 11:24:06 +0200

    Plot rain data for Sitka Highs

--- weather_data/sitka_rainfall.py
+++ weather_data/sitka_rainfall.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import csv
 
+import matplotlib.pyplot as plt
+
 path = Path('./sitka_weather_2021_full.csv')
 lines = path.read_text().splitlines()
 
@@ -10,5 +12,16 @@ header_row = next(reader)
 # Extract rainfall data.
 rain = [float(row[5]) for row in reader]
 
-print(rain)
+# Plot the rain data.
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+ax.plot(rain, color='dodgerblue')
+
+# Format plot.
+ax.set_title("Daily precipitation maximum, 2021\nSitka Highs, AK", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel("PRCP", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
 
