12571d384465a459fc75a0f1147c4a5900ab8b01
emiliano <echiper.dev@gmail.com>
2025-01-20 12:01:55 +0200

    Summarize top repositories

--- api_visualization/python_repos.py
+++ api_visualization/python_repos.py
@@ -19,14 +19,13 @@ repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 
 # Examine the first repository.
-repo_dict = repo_dicts[0]
+repo_dicts = response_dict['items']
 
-print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+for repo_dict in repo_dicts:
+    print("\nSelected information about first repository:")
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}")
       
